40684f2995bcc8163297c15be231388433bc50f6
Brian Hirsh <hirsheybar@fb.com>
2020-11-18 12:20:21 -0800

    [TEST] make validate debug-only in Device copy ctr
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/47854
    
    Differential Revision: [D25003113](https://our.internmc.facebook.com/intern/diff/D25003113/)
    ghstack-source-id: 117036805

--- test/test_torch.py
+++ test/test_torch.py
@@ -696,28 +696,6 @@ class AbstractTestCases:
             self.assertEqual('cuda', cuda23333.type)
             self.assertEqual(23333, cuda23333.index)
 
-            self.assertRaises(RuntimeError, lambda: torch.device('cpu:-1'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cpu:1'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cpu', -1))
-            self.assertRaises(RuntimeError, lambda: torch.device('cpu', 1))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:-1'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:2 '))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda: 2'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:2 2'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:2.'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:2?'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:?2'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:2.232'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:2 cuda:3'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:2+cuda:3'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda:2cuda:3'))
-            self.assertRaises(RuntimeError, lambda: torch.device('cuda', -1))
-            self.assertRaises(RuntimeError, lambda: torch.device(-1))
-
-            self.assertRaises(RuntimeError, lambda: torch.device('other'))
-            self.assertRaises(RuntimeError, lambda: torch.device('other:0'))
-
             device_set = {'cpu', 'cpu:0', 'cuda', 'cuda:0', 'cuda:1', 'cuda:10', 'cuda:100'}
             device_hash_set = set()
             for device in list(device_set):
